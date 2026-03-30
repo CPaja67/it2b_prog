3d0e0e13269f633dfe236571d468f8915ba1fe27
CPaja67 <st026332@student.cichnovabrno.cz>
2026-03-30 15:08:27 +0200

    asdasda

--- CPaja_Prace/C#/program/program/Program.cs
+++ CPaja_Prace/C#/program/program/Program.cs
@@ -1,22 +1,51 @@
 ﻿class Program
 {
-    static void Main()
+
+    static void GrafickeZnazorneni(int[] indexy, int vyska)
     {
-        double[] teploty = new double[10];
-        Console.WriteLine("zadej 10 teplot (desetiny) po sobe:");
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < vyska; i++)
         {
-            Console.Write($"teplota {i + 1}: ");
-            teploty[i] = double.Parse(Console.ReadLine());
+            for (int j = 0; j < indexy.Length; j++)
+            {
+                if (indexy[i] == j)
+                {
+                    Console.Write("O");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
             Console.WriteLine("");
         }
-        Console.WriteLine("---------------------");
-        Array.Sort(teploty);
-        Console.WriteLine($"maximum je {teploty.Max()}");
-        Console.WriteLine($"rozdil mezi maximem a minimem je {teploty.Max() - teploty.Min()}");
-        Console.WriteLine("nejnizsi 3 hodnoty jsou: ");
-        for (int x = 0; x <3; x++) Console.WriteLine($" --> {teploty[x]}");
-        Console.WriteLine("");
-        Console.WriteLine($"Median je: {(teploty[4] + teploty[5]) / 2}");
     }
+        static void Vykreslit(int[] hodnoty)
+        {
+
+            int[] indexy = new int[hodnoty.Length];
+            int[] serazeno = new int[hodnoty.Length];
+            Array.Copy(hodnoty, serazeno, hodnoty.Length);
+            Array.Sort(serazeno);
+            Array.Reverse(serazeno);
+
+            for (int i = 0; i < hodnoty.Length; i++)
+            {
+                for (int j = 0; j < serazeno.Length; j++)
+                {
+                    if (serazeno[i] == hodnoty[j])
+                    {
+                        if (!indexy.Contains(hodnoty[i])) indexy[i] = j;
+                    }
+                }
+            }
+            GrafickeZnazorneni(indexy, hodnoty.Max());
+        }
+
+        static void Main()
+        {
+            int[] hodnoty = { 0, 1, 2, 3, 4, 5, 6, 7 ,8 ,9, 10};
+            Vykreslit(hodnoty);
+
+        }
+    
 }
--- CPaja_Prace/C#/program/program/obj/Debug/net8.0/program.AssemblyInfo.cs
+++ CPaja_Prace/C#/program/program/obj/Debug/net8.0/program.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b250a63036f327ac9895f6ce2b40a37d64acfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f23b2d54360e674ee259a5fac34d5559ed0b2a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
